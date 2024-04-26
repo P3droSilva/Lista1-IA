90aa76c5a009f7fe3fc16847b060a4bcddd0656c
P3dro_Silva <phhinerasky@inf.ufsm.br>
2024-04-26 13:19:05 -0300

    Exercicios 1 e 4

--- exercicio1.py
+++ exercicio1.py
@@ -1,84 +1,209 @@
-class Estado:
+from collections import deque
+
+class Vertice:
     def __init__(self, missionarios, canibais, barco):
         self.missionarios = missionarios
         self.canibais = canibais
         self.barco = barco
-        self.pai = None
+        self.adjacentes = []
+        self.visitado = False
 
-    def __eq__(self, other):
-        return self.missionarios == other.missionarios and \
-               self.canibais == other.canibais and \
-               self.barco == other.barco
-
-    def __hash__(self):
-        return hash((self.missionarios, self.canibais, self.barco))
-
-    def __str__(self):
-        return f'M: {self.missionarios}, C: {self.canibais}, B: {self.barco}'
-
-    def __repr__(self):
-        return str(self)
+    def adiciona_adjacente(self, vertice):
+        if vertice not in self.adjacentes:
+            self.adjacentes.append(vertice)
 
 
-def validar_estado(estado):
-    if estado.missionarios < 0 or estado.canibais < 0 or \
-       estado.missionarios > N or estado.canibais > N or \
-       (estado.missionarios != 0 and estado.missionarios < estado.canibais) or \
-       (estado.missionarios != N and N - estado.missionarios < N - estado.canibais):
-        return False
-    return True
+class Grafo:
+    def __init__(self, maxMissionarios, maxCanibais):
+        self.vertices = []
+        self.maxMissionarios = maxMissionarios
+        self.maxCanibais = maxCanibais
 
+    def adiciona_vertice(self, vertice):
+        self.vertices.append(vertice)
 
-def gerar_filhos(estado):
-    filhos = []
-    for i in range(M + 1):
-        for j in range(C + 1):
-            if 1 <= i + j <= O and (i > 0 or j > 0):
-                if estado.barco == 1:
-                    novo_estado = Estado(estado.missionarios - i, estado.canibais - j, 0)
-                else:
-                    novo_estado = Estado(estado.missionarios + i, estado.canibais + j, 1)
-                novo_estado.pai = estado
-                if validar_estado(novo_estado):
-                    filhos.append(novo_estado)
-    return filhos
+    def adiciona_aresta(self, vertice_origem, vertice_destino):
+        vertice_origem.adiciona_adjacente(vertice_destino)
 
+    def cria_vertices(self):
+        for missionarios in range(self.maxMissionarios + 1):
+            for canibais in range(self.maxCanibais + 1):
+                for barco in ["esq", "dir"]:
+                    vertice = Vertice(missionarios, canibais, barco)
+                    self.adiciona_vertice(vertice)
 
-def busca_largura():
-    inicial = Estado(N, N, 1)
-    if not validar_estado(inicial):
+    def cria_arestas(self):
+        for vertice in self.vertices:
+            for acao in [levar1M, levar2M, levar1C, levar2C, levar1M1C, voltar1M, voltar2M, voltar1C, voltar2C, voltar1M1C]:
+                novo_estado = acao(vertice, self.maxMissionarios, self.maxCanibais)
+                if novo_estado:
+                    novo_vertice = self.procura_estado(novo_estado)
+                    if novo_vertice and estado_valido(novo_vertice, self.maxMissionarios, self.maxCanibais):
+                        self.adiciona_aresta(vertice, novo_vertice)
+
+    def procura_estado(self, estado):
+        for vertice in self.vertices:
+            if vertice.missionarios == estado[0] and vertice.canibais == estado[1] and vertice.barco == estado[2]:
+                return vertice
         return None
 
-    visitados = set()
-    fila = [inicial]
+    def print_grafo(self):
+        for vertice in self.vertices:
+            print("Missionarios {} Canibais {} Barco {}".format(vertice.missionarios, vertice.canibais, vertice.barco))
+            print("  Adjacentes:")
+            for adjacente in vertice.adjacentes:
+                print("    Missionarios {} Canibais {} Barco {}".format(adjacente.missionarios, adjacente.canibais, adjacente.barco))
+            print()
 
-    while fila:
-        estado_atual = fila.pop(0)
-        visitados.add(estado_atual)
+class Resolucao:
 
-        if estado_atual.missionarios == 0 and estado_atual.canibais == 0 and estado_atual.barco == 0:
-            return estado_atual
+    def __init__(self, N):
+        self.grafo = Grafo(N, N)
+        self.grafo.cria_vertices()
+        self.grafo.cria_arestas()
+        self.estado_inicial = None
+        self.estado_final = None
 
-        filhos = gerar_filhos(estado_atual)
-        for filho in filhos:
-            if filho not in visitados and filho not in fila:
-                fila.append(filho)
+        for vertice in self.grafo.vertices:
+            if vertice.missionarios == N and vertice.canibais == N and vertice.barco == "esq":
+                self.estado_inicial = vertice
 
-    return None
+            if vertice.missionarios == 0 and vertice.canibais == 0 and vertice.barco == "dir":
+                self.estado_final = vertice
+
+        #self.grafo.print_grafo()
 
 
-N = 3  # Número de missionários e canibais
-M = C = N  # Número máximo de missionários e canibais em cada lado
-O = 2  # Número máximo de pessoas no barco
+    def busca_em_largura(self):
+        fila = deque([(self.estado_inicial, [])])
+        nos_abertos = []
+        nos_fechados = []
+        arvore_busca = {self.estado_inicial: []}
 
-solucao = busca_largura()
-if solucao:
-    caminho = []
-    while solucao:
-        caminho.append(solucao)
-        solucao = solucao.pai
-    caminho.reverse()
-    for estado in caminho:
-        print(estado)
-else:
-    print("Não há solução possível.")
+        while fila:
+            vertice_atual, passo_a_passo_atual = fila.popleft()
+            nos_abertos.append(vertice_atual)
+            vertice_atual.visitado = True
+
+            if vertice_atual == self.estado_final:
+                return passo_a_passo_atual, nos_abertos, nos_fechados, arvore_busca
+            
+            for adjacente in vertice_atual.adjacentes:
+                if not adjacente.visitado:
+                    fila.append((adjacente, passo_a_passo_atual + [adjacente]))
+                    arvore_busca.setdefault(vertice_atual, [])
+                    arvore_busca[vertice_atual].append(adjacente)
+                else:
+                    nos_fechados.append(adjacente)
+
+        return passo_a_passo_atual ,nos_abertos, nos_fechados, arvore_busca
+
+    def busca_em_profundidade(self):
+        pilha = [(self.estado_inicial, [])]
+        nos_abertos = []
+        nos_fechados = []
+        arvore_busca = {self.estado_inicial: []}
+
+        while pilha:
+            vertice_atual, passo_a_passo_atual = pilha.pop()
+            nos_abertos.append(vertice_atual)
+            vertice_atual.visitado = True
+
+            if vertice_atual == self.estado_final:
+                return passo_a_passo_atual, nos_abertos, nos_fechados, arvore_busca
+
+            for adjacente in vertice_atual.adjacentes:
+                if not adjacente.visitado:
+                    pilha.append((adjacente, passo_a_passo_atual + [adjacente]))
+                    arvore_busca.setdefault(vertice_atual, [])
+                    arvore_busca[vertice_atual].append(adjacente)
+                else:
+                    nos_fechados.append(adjacente)
+
+        return passo_a_passo_atual, nos_abertos, nos_fechados, arvore_busca
+    
+
+def estado_valido(vertice, maxMissionarios, maxCanibais):
+    if vertice.missionarios < 0 or vertice.missionarios > maxMissionarios:
+        return False
+    
+    if vertice.canibais < 0 or vertice.canibais > maxCanibais:
+        return False
+    
+    if vertice.missionarios < vertice.canibais and vertice.missionarios > 0:
+        return False
+    
+    if maxCanibais - vertice.canibais > maxMissionarios - vertice.missionarios and maxMissionarios - vertice.missionarios > 0:
+        return False
+    
+    return True
+
+def levar1M(vertice, maxMissionarios, maxCanibais):
+    if vertice.barco == "esq" and vertice.missionarios >= 1:
+        return [vertice.missionarios - 1, vertice.canibais, "dir"]
+    
+def levar2M(vertice, maxMissionarios, maxCanibais):
+    if vertice.barco == "esq" and vertice.missionarios >= 2:
+        return [vertice.missionarios - 2, vertice.canibais, "dir"]
+    
+def levar1C(vertice, maxMissionarios, maxCanibais):
+    if vertice.barco == "esq" and vertice.canibais >= 1:
+        return [vertice.missionarios, vertice.canibais - 1, "dir"]
+    
+def levar2C(vertice, maxMissionarios, maxCanibais):
+    if vertice.barco == "esq" and vertice.canibais >= 2:
+        return [vertice.missionarios, vertice.canibais - 2, "dir"]
+    
+def levar1M1C(vertice, maxMissionarios, maxCanibais):
+    if vertice.barco == "esq" and vertice.missionarios >= 1 and vertice.canibais >= 1:
+        return [vertice.missionarios - 1, vertice.canibais - 1, "dir"]
+    
+def voltar1M(vertice, maxMissionarios, maxCanibais):
+    if vertice.barco == "dir" and maxMissionarios - vertice.missionarios >= 1:
+        return [vertice.missionarios + 1, vertice.canibais, "esq"]
+    
+def voltar2M(vertice, maxMissionarios, maxCanibais):
+    if vertice.barco == "dir" and maxMissionarios - vertice.missionarios >= 2:
+        return [vertice.missionarios + 2, vertice.canibais, "esq"]
+    
+def voltar1C(vertice, maxMissionarios, maxCanibais):
+    if vertice.barco == "dir" and maxCanibais - vertice.canibais >= 1:
+        return [vertice.missionarios, vertice.canibais + 1, "esq"]
+    
+def voltar2C(vertice, maxMissionarios, maxCanibais):
+    if vertice.barco == "dir" and maxCanibais - vertice.canibais >= 2:
+        return [vertice.missionarios, vertice.canibais + 2, "esq"]
+    
+def voltar1M1C(vertice, maxMissionarios, maxCanibais):
+    if vertice.barco == "dir" and maxMissionarios - vertice.missionarios >= 1 and maxCanibais - vertice.canibais >= 1:
+        return [vertice.missionarios + 1, vertice.canibais + 1, "esq"]
+    
+
+
+
+
+
+# Executando a busca em largura e imprimindo o resultado
+N = 3
+resolucao = Resolucao(N)
+passo_a_passo, nos_abertos, nos_fechados, arvore_busca = resolucao.busca_em_largura()
+
+print("\nPasso a passo: ")
+for vertice in passo_a_passo:
+    print("  Esq: Missionarios {} Canibais {}\n  Barco {}\n  Dir: Missionarios {} Canibais {}\n".format(vertice.missionarios, vertice.canibais, vertice.barco, N - vertice.missionarios, N - vertice.canibais))
+
+print("\nNós abertos:")
+for vertice in nos_abertos:
+    print("  Missionarios {} Canibais {} Barco {}".format(vertice.missionarios, vertice.canibais, vertice.barco))
+
+print("\nNós fechados:")
+for vertice in nos_fechados:
+    print("  Missionarios {} Canibais {} Barco {}".format(vertice.missionarios, vertice.canibais, vertice.barco))
+
+print("\nÁrvore de busca:")
+for vertice, adjacentes in arvore_busca.items():
+    print("  Missionarios {} Canibais {} Barco {}".format(vertice.missionarios, vertice.canibais, vertice.barco))
+    print("    Adjacentes:")
+    for adjacente in adjacentes:
+        print("      Missionarios {} Canibais {} Barco {}".format(adjacente.missionarios, adjacente.canibais, adjacente.barco))
+    print()
---
+++ exercicio4.py
@@ -0,0 +1,259 @@
+from collections import deque
+
+class Vertice:
+    def __init__(self, cidade):
+        self.cidade = cidade
+        self.adjacentes = []
+        self.visitado = False
+
+    def adiciona_adjacente(self, vertice):
+        if vertice not in self.adjacentes:
+            self.adjacentes.append(vertice)
+
+
+class Grafo:
+    def __init__(self):
+        self.vertices = []
+
+    def adiciona_vertice(self, vertice):
+        self.vertices.append(vertice)
+
+    def adiciona_aresta(self, vertice_origem, vertice_destino):
+        vertice_origem.adiciona_adjacente(vertice_destino)
+
+    def reset_visitas(self):
+        for vertice in self.vertices:
+            vertice.visitado = False
+
+    def cria_vertices(self):
+        cidades = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"]
+        for cidade in cidades:
+            vertice = Vertice(cidade)
+            self.adiciona_vertice(vertice)
+
+    def cria_arestas(self):
+        for vertice in self.vertices:
+            for acao in [oper1, oper3, oper4, oper5, oper6, oper7, oper8, oper9, oper10, oper11, oper12, oper13, oper14, oper15, oper16, oper17, oper18, oper19]:
+                novo_estado = acao(vertice)
+                if novo_estado:
+                    novo_vertice = self.procura_estado(novo_estado)
+                    if novo_vertice and estado_valido(novo_vertice):
+                        self.adiciona_aresta(vertice, novo_vertice)
+
+    def procura_estado(self, estado):
+        for vertice in self.vertices:
+            if vertice.cidade == estado:
+                return vertice
+        return None
+
+    def print_grafo(self):
+        for vertice in self.vertices:
+            print("Cidade {}".format(vertice.cidade))
+            print("  Adjacentes:")
+            for adjacente in vertice.adjacentes:
+                print("    Cidade {}".format(adjacente.cidade))
+            print()
+
+class Resolucao:
+
+    def __init__(self, estado_inicial, estado_final):
+        self.grafo = Grafo()
+        self.grafo.cria_vertices()
+        self.grafo.cria_arestas()
+        self.estado_inicial = None
+        self.estado_final = None
+
+        for vertice in self.grafo.vertices:
+            if vertice.cidade == estado_inicial:
+                self.estado_inicial = vertice
+
+            if vertice.cidade == estado_final:
+                self.estado_final = vertice
+
+        #self.grafo.print_grafo()
+
+
+    def busca_em_largura(self):
+        fila = deque([(self.estado_inicial, [])])
+        nos_abertos = []
+        nos_fechados = []
+        arvore_busca = {self.estado_inicial: []}
+
+        while fila:
+            vertice_atual, passo_a_passo_atual = fila.popleft()
+            nos_abertos.append(vertice_atual)
+            vertice_atual.visitado = True
+
+            if vertice_atual == self.estado_final:
+                self.grafo.reset_visitas()
+                return passo_a_passo_atual, nos_abertos, nos_fechados, arvore_busca
+            
+            for adjacente in vertice_atual.adjacentes:
+                if not adjacente.visitado:
+                    fila.append((adjacente, passo_a_passo_atual + [adjacente]))
+                    arvore_busca.setdefault(vertice_atual, [])
+                    arvore_busca[vertice_atual].append(adjacente)
+                else:
+                    nos_fechados.append(adjacente)
+        
+        self.grafo.reset_visitas()
+        return passo_a_passo_atual ,nos_abertos, nos_fechados, arvore_busca
+
+    def busca_em_profundidade(self):
+        pilha = [(self.estado_inicial, [])]
+        nos_abertos = []
+        nos_fechados = []
+        arvore_busca = {self.estado_inicial: []}
+
+        while pilha:
+            vertice_atual, passo_a_passo_atual = pilha.pop()
+            nos_abertos.append(vertice_atual)
+            vertice_atual.visitado = True
+
+            if vertice_atual == self.estado_final:
+                self.grafo.reset_visitas()
+                return passo_a_passo_atual, nos_abertos, nos_fechados, arvore_busca
+
+            for adjacente in vertice_atual.adjacentes:
+                if not adjacente.visitado:
+                    pilha.append((adjacente, passo_a_passo_atual + [adjacente]))
+                    arvore_busca.setdefault(vertice_atual, [])
+                    arvore_busca[vertice_atual].append(adjacente)
+                else:
+                    nos_fechados.append(adjacente)
+
+        self.grafo.reset_visitas()
+        return passo_a_passo_atual, nos_abertos, nos_fechados, arvore_busca
+    
+
+def estado_valido(vertice):
+    if vertice.cidade in ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"]:
+        return True
+
+def oper1(vertice):
+    if vertice.cidade == "A":
+        return "B"
+    
+def oper3(vertice):
+    if vertice.cidade == "A":
+        return "D"
+
+def oper4(vertice):
+    if vertice.cidade == "B":
+        return "E"
+    
+def oper5(vertice):
+    if vertice.cidade == "B":
+        return "F"
+    
+def oper6(vertice):
+    if vertice.cidade == "C":
+        return "G"
+    
+def oper7(vertice):
+    if vertice.cidade == "C":
+        return "H"
+    
+def oper8(vertice):
+    if vertice.cidade == "C":
+        return "I"
+    
+def oper9(vertice):
+    if vertice.cidade == "D":
+        return "J"
+    
+def oper10(vertice):
+    if vertice.cidade == "E":
+        return "K"
+    
+def oper11(vertice):
+    if vertice.cidade == "E":
+        return "L"
+    
+def oper12(vertice):
+    if vertice.cidade == "G":
+        return "M"
+    
+def oper13(vertice):
+    if vertice.cidade == "J":
+        return "N"
+    
+def oper14(vertice):
+    if vertice.cidade == "J":
+        return "O"
+    
+def oper15(vertice):
+    if vertice.cidade == "K":
+        return "F"
+    
+def oper16(vertice):
+    if vertice.cidade == "L":
+        return "H"
+    
+def oper17(vertice):
+    if vertice.cidade == "M":
+        return "D"
+
+def oper18(vertice):
+    if vertice.cidade == "O":
+        return "A"
+    
+def oper19(vertice):
+    if vertice.cidade == "N":
+        return "B"
+
+# Definindo o estado inicial e final
+estado_inicial = "A"
+estado_final = "J"
+resolucao = Resolucao(estado_inicial, estado_final)
+
+# Executando a busca em largura e imprimindo o resultado
+passo_a_passo, nos_abertos, nos_fechados, arvore_busca = resolucao.busca_em_largura()
+
+print("\n\n-----------------BUSCA EM LARGURA------------------\n\n")
+
+print("\nPasso a passo: ")
+for vertice in passo_a_passo:
+    print("  Cidade {}".format(vertice.cidade))
+
+print("\nNós abertos:")
+for vertice in nos_abertos:
+    print("  Cidade {}".format(vertice.cidade))
+
+print("\nNós fechados:")
+for vertice in nos_fechados:
+    print("  Cidade {}".format(vertice.cidade))
+
+print("\nÁrvore de busca:")
+for vertice, adjacentes in arvore_busca.items():
+    print("  Cidade {}".format(vertice.cidade))
+    print("    Adjacentes:")
+    for adjacente in adjacentes:
+        print("      Cidade {}".format(adjacente.cidade))
+    print()
+
+
+# Executando a busca em profundidade e imprimindo o resultado
+passo_a_passo, nos_abertos, nos_fechados, arvore_busca = resolucao.busca_em_profundidade()
+
+print("\n\n-----------------BUSCA EM PROFUNDIDADE------------------\n\n")
+
+print("\nPasso a passo: ")
+for vertice in passo_a_passo:
+    print("  Cidade {}".format(vertice.cidade))
+
+print("\nNós abertos:")
+for vertice in nos_abertos:
+    print("  Cidade {}".format(vertice.cidade))
+
+print("\nNós fechados:")
+for vertice in nos_fechados:
+    print("  Cidade {}".format(vertice.cidade))
+
+print("\nÁrvore de busca:")
+for vertice, adjacentes in arvore_busca.items():
+    print("  Cidade {}".format(vertice.cidade))
+    print("    Adjacentes:")
+    for adjacente in adjacentes:
+        print("      Cidade {}".format(adjacente.cidade))
+    print()
